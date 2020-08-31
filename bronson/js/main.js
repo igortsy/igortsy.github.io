15f9c77e56a8637ba6e7e23a82907980ed4b1430
igortsy <61879791+igortsy@users.noreply.github.com>
2020-08-31 19:44:49 +0300

    Update main.js

--- bronson/js/main.js
+++ bronson/js/main.js
@@ -1,30 +1,59 @@
-$(function(){
-    setTimeout(function(){$('.preloader').css({visibility:'hidden',opacity:0})},1500);
+$(function() {
+	setTimeout(function(){$('.preloader').css({visibility:'hidden',opacity:0})},1500)
 
     $(window).on('scroll', function() {
-        if (window.pageYOffset > 50) {
-        		$('.header').addClass('stick');
-        		$('.header__burger').addClass('dark');
+    	//stick header------------------------------------
+        if ($(window).scrollTop() > 50) {
+            $('.header').addClass('stick');
+            $('.header__burger').addClass('dark');
         } else {
             $('.header').removeClass('stick');
             $('.header__burger').removeClass('dark');
         }
+      //pointway menu----------------------------------------
+        let windowOffset = $(window).scrollTop();
+        if (Math.floor($('#about').offset().top) <= windowOffset && Math.floor($('#services').offset().top) > windowOffset) {
+            $('.header__nav-link').removeClass('active');
+            $('a[href="#about"]').addClass('active');
+        } else if (Math.floor($('#services').offset().top) <= windowOffset && Math.floor($('#portfolio').offset().top) > windowOffset) {
+            $('.header__nav-link').removeClass('active');
+            $('a[href="#services"]').addClass('active');
+        } else if (Math.floor($('#portfolio').offset().top) <= windowOffset && Math.floor($('#blog').offset().top) > windowOffset) {
+            $('.header__nav-link').removeClass('active');
+            $('a[href="#portfolio"]').addClass('active');
+        } else if (Math.floor($('#blog').offset().top) <= windowOffset && Math.floor($('#contact').offset().top) - 200 > windowOffset) {
+            $('.header__nav-link').removeClass('active');
+            $('a[href="#blog"]').addClass('active');
+        } else if (Math.floor($('#contact').offset().top - 200) <= windowOffset) {
+            $('.header__nav-link').removeClass('active');
+            $('a[href="#contact"]').addClass('active');
+        } else {
+            $('.header__nav-link').removeClass('active');
+            $('a[href="#home"]').addClass('active');
+        }
     });
-	$('form').validate({
-		rules:{
-			email:{
-				required: true,
-				email: true
-			},
-			topic:{
-				required: true,
-			}
-		}
-	});
+    //form validate---------------------------------------------------
+    $('form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+            topic: {
+                required: true,
+            }
+        }
+    });
+    //typed plagin-----------------------------------------------------
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        loop: true,
+        typeSpeed: 60,
+        startDelay: 30,
+    });
 
-	var typed = new Typed('#typed',{stringsElement:'#typed-strings',loop:true,typeSpeed:60, startDelay: 30,});
-
-	   var $grid = $('.izo-box').isotope({
+    //izotope--------------------------------------------------------
+    var $grid = $('.izo-box').isotope({
         itemSelector: '.cell',
         layoutMode: 'fitRows'
     });
@@ -43,7 +72,7 @@ $(function(){
     });
 
 
-
+    //popup-----------------------------------------------------------
     $('.izo-box').magnificPopup({
         delegate: 'a.filter',
         type: 'image',
@@ -62,80 +91,35 @@ $(function(){
         }
     });
 
-   var owl = $('.owl-carousel');
-owl.owlCarousel({items:2,margin:30,dots:true,
-responsive : {
-    // breakpoint from 0 up
-    0 : {
-    	items:1
-    },
-    // breakpoint from 480 up
-    480 : {
-    	items:1
-    },
-    // breakpoint from 768 up
-    768 : {
-		items:2
-    }
-}
-});
-
-//плавная прокрутка-------------------------------------------
-
-    $(".header__nav").on("click","a", function (event) {
-        // исключаем стандартную реакцию браузера
-        event.preventDefault();
-
-        // получем идентификатор блока из атрибута href
-        let id  = $(this).attr('href');
-
-        // находим высоту, на которой расположен блок
-        let top = $(id).offset().top;
-
-        // анимируем переход к блоку, время: 800 мс
-        $('body,html').animate({scrollTop: top}, 800);
-        	$('.header__burger,.header__nav').toggleClass('active');
+    //Owl карусель
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 2,
+        margin: 30,
+        dots: true,
+        responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 1
+            },
+            // breakpoint from 768 up
+            768: {
+                items: 2
+            }
+        }
     });
-
-    let home = document.querySelector('#home');
-    let about = document.querySelector('#about');
-    let services = document.querySelector('#services');
-    let portfolio = document.querySelector('#portfolio');
-    let blog = document.querySelector('#blog');
-    let contact = document.querySelector('#contact');
-
-    window.addEventListener("scroll",()=>{
-    	let windo = window.pageYOffset;
-    	if(about.offsetTop <= windo && services.offsetTop > windo){
-    		$('.header__nav-link').removeClass('active');
-    		$('a[href="#about"]').addClass('active');
-    	}
-    	else if(services.offsetTop <= windo && portfolio.offsetTop > windo){
-    		$('.header__nav-link').removeClass('active');
-    		$('a[href="#services"]').addClass('active');
-    	}
-    	else if(portfolio.offsetTop <= windo && blog.offsetTop > windo){
-    		$('.header__nav-link').removeClass('active');
-    		$('a[href="#portfolio"]').addClass('active');
-    	}
-    	else if(blog.offsetTop <= windo && contact.offsetTop-200 > windo){
-    		$('.header__nav-link').removeClass('active');
-    		$('a[href="#blog"]').addClass('active');
-    	}
-    	else if(contact.offsetTop-200 <= windo){
-    		$('.header__nav-link').removeClass('active');
-    		$('a[href="#contact"]').addClass('active');
-    	}
-    	else{
-    		$('.header__nav-link').removeClass('active');
-    		$('a[href="#home"]').addClass('active');
-    	}
-    })
-
-            $('.header__burger').on('click',function(e){
-        	$('body,html').css('overflow-y','auto');
-        	$('.header__burger,.header__nav').toggleClass('active');
-
-
-        });
+    //плавная прокрутка-------------------------------------------
+    $(".header__nav").on("click", "a", function(event) {
+        event.preventDefault();
+        let id = $(this).attr('href');
+        let top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 800);
+        $('.header__burger,.header__nav').toggleClass('active');
+    });
+    $('.header__burger').on('click', function(e) {
+        $('body,html').css('overflow-y', 'auto');
+        $('.header__burger,.header__nav').toggleClass('active');
+    });
 });
